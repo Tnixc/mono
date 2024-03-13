2156c4c2208f0ecbb6c8c53d5473ff498fa7e907
Tnixc <tnixxc@gmail.com>
2024-03-12 21:43:46 -0400

    more stuff

--- test.js
+++ test.js
@@ -1,19 +1,24 @@
 import * as bun from 'bun';
 
 const API_URL = 'http://192.168.2.97:9000/asr';
-const WAV_PATH = 'chut.wav'
+const WAV_PATH = 'ch.mp3'
+const params = new URLSearchParams({
+  task: 'transcribe',
+  output: 'json',
+  word_timestamps: 'true',
+});
 
 const formData = new FormData();
 formData.append('audio_file', bun.file(WAV_PATH));
 
-const response = await bun.fetch(`${API_URL}?output=json&word_timestamps=true`, {
+const response = await bun.fetch(`${API_URL}?${params.toString()}`, {
   method: 'POST',
   body: formData,
 });
 
 if (response.ok) {
   const data = await response.text();
-  console.log('Transcription:', data);
+  console.log(data);
 } else {
-  console.error('Error:', response.status, response.statusText);
+  console.error(response.status, response.statusText);
 }
